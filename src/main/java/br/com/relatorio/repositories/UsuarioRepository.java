86666fc0c16aacf95da5829bd705d26af2b5b9b2
posfiapteam35@gmail.com <posfiapteam35@gmail.com>
2025-11-16 00:07:35 -0300

    Higienização do código.

--- src/main/java/br/com/relatorio/repositories/UsuarioRepository.java
+++ src/main/java/br/com/relatorio/repositories/UsuarioRepository.java
@@ -3,18 +3,12 @@ package br.com.relatorio.repositories;
 import java.util.List;
 
 import br.com.relatorio.entities.db.UsuarioDB;
-import br.com.relatorio.repositories.interfaces.IUsuarioDbRepository;
 import br.com.relatorio.repositories.interfaces.IUsuarioRepository;
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
 
 @RequestScoped
 public class UsuarioRepository implements IUsuarioRepository{
 
-	//@Inject
-	private IUsuarioDbRepository usuarioRepository;
-	
 	@Override
 	public List<UsuarioDB> buscarUsuarios() {
 		return UsuarioDB.buscarUsuariosAdministradores();
