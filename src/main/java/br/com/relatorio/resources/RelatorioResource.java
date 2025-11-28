d882dd390e884f7924384b96335dd07545d92536
THIAGO MOTTA DE SOUZA <F3501132@portoseguro.com.br>
2025-11-28 13:39:00 -0300

    Adicionando a versão do azure funtions

--- src/main/java/br/com/relatorio/resources/RelatorioResource.java
+++ src/main/java/br/com/relatorio/resources/RelatorioResource.java
@@ -1,24 +1,22 @@
 package br.com.relatorio.resources;
 
-import br.com.relatorio.services.interfaces.IRelatorioService;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import java.util.List;
-import java.util.ArrayList;
-import br.com.relatorio.services.ResumoAvaliacoesService;
-import br.com.relatorio.services.AvaliacoesFrequentesService;
-import jakarta.enterprise.inject.Instance;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
 
-@Path("/ensino/relatorios")
+import br.com.relatorio.services.interfaces.IRelatorioService;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+
+//@Path("/ensino/relatorios")
 public class RelatorioResource {
 
 	@Inject
 	private Instance<IRelatorioService> relatoriosService;
 	
-    @POST
-    @Path("/processar")
-    public void processarRelatorios() {
+    //@POST
+    //@Path("/processar")
+    @FunctionName("fnc-ensinorelatorio")
+    public void processarRelatorios(@TimerTrigger(name = "agendamento", schedule = "0 */1 * * * *") String agendamento) {
     	relatoriosService.stream().forEach(IRelatorioService::gerarRelatorio);
     }
 }
