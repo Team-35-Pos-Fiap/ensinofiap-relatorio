28bc9e9981f0120fc7d91da7fffa383553d10c2e
posfiapteam35@gmail.com <posfiapteam35@gmail.com>
2025-12-11 11:19:56 -0300

    Ajuste na função cron

--- src/main/java/br/com/relatorio/resources/RelatorioResource.java
+++ src/main/java/br/com/relatorio/resources/RelatorioResource.java
@@ -1,13 +1,12 @@
 package br.com.relatorio.resources;
 
+import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.TimerTrigger;
 
 import br.com.relatorio.services.interfaces.IRelatorioService;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
 
 //@Path("/ensino/relatorios")
 public class RelatorioResource {
@@ -16,7 +15,7 @@ public class RelatorioResource {
 	private Instance<IRelatorioService> relatoriosService;
 	
     @FunctionName("fnc-relatorio-avaliacoes")
-    public void processarRelatorios(@TimerTrigger(name = "agendamento", schedule = "0 * * ? * *") String agendamento) {
+    public void processarRelatorios(@TimerTrigger(name = "agendamento", schedule = "0 */1 * * * *") String agendamento, final ExecutionContext execucao) {
     	relatoriosService.stream().forEach(IRelatorioService::gerarRelatorio);
     }
 
