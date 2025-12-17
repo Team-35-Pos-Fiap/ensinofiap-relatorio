1ef5367eaf94912e9c3bd0ce2f9a7497de770c2b
Jedaías Correia de França <jhednos@gmail.com>
2025-12-17 01:13:09 -0300

    update test funtion relatorio

--- src/main/java/br/com/relatorio/resources/RelatorioResource.java
+++ src/main/java/br/com/relatorio/resources/RelatorioResource.java
@@ -1,21 +1,41 @@
 package br.com.relatorio.resources;
 
 import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
 import com.microsoft.azure.functions.annotation.TimerTrigger;
 
 import br.com.relatorio.services.interfaces.IRelatorioService;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+import java.util.Optional;
 
 //@Path("/ensino/relatorios")
 public class RelatorioResource {
 
 	@Inject
 	private Instance<IRelatorioService> relatoriosService;
-	
+
+//    @POST
     @FunctionName("fnc-relatorio-avaliacoes")
-    public void processarRelatorios(@TimerTrigger(name = "agendamento", schedule = "0 */1 * * * *") String agendamento, final ExecutionContext execucao) {
+    public void processarRelatorios(
+            @HttpTrigger(
+                    name = "req",
+                    methods = {HttpMethod.GET, HttpMethod.POST},
+                    authLevel = AuthorizationLevel.ANONYMOUS)
+            @TimerTrigger(name = "agendamento", schedule = "0 */1 * * * *")
+            String agendamento, final ExecutionContext execucao,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+//    public void processarRelatorios(@TimerTrigger(name = "agendamento", schedule = "0 */1 * * * *") String agendamento, final ExecutionContext execucao) {
     	relatoriosService.stream().forEach(IRelatorioService::gerarRelatorio);
     }
 
